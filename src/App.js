87afe3b4baa277645cc9c5a7b8c27062e65f9030
Shoaib ulla khan <shoaibullakhan1999@outlook.com>
2024-05-08 11:57:30 +0530

    updated app.js and added totalAmount component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import BillDetails from "./components/BillDetails";
 
 function App() {
   return (
     <div className="App">
-      <div>App</div>
+      <div>
+        <BillDetails />
+      </div>
     </div>
   );
 }
