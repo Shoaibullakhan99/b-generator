fa739d82b6ac5cf9df29c9cc342ef43c97aa5ab1
Shoaib ulla khan <shoaibullakhan1999@outlook.com>
2024-05-08 12:05:18 +0530

    updated app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
+import { useState } from "react";
 import BillDetails from "./components/BillDetails";
+import ItemList from "./components/ItemList";
+import TotalAmount from "./components/TotalAmount";
 
 function App() {
+  const [items, setItems] = useState([]);
+
+
   return (
     <div className="App">
-      <div>
-        <BillDetails />
-      </div>
+        <h1>Bill / Invoice Generator</h1>
+        <BillDetails  onAddItem={{handleAddition}}/>
+        <ItemList items={items} onDeleteItem={{handleDeletion}} />
+        <TotalAmount total={calculateTotalAmount()} />
+        <button onClick={handleDownloadPDF}>Download PDF</button>
     </div>
   );
 }
